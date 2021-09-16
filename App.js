6f1c2922db01527e74d3be0a121a6ffdd54f3da5
Poul <poulnichols@outlook.com>
2021-09-15 20:31:50 -0700

    checkpoint

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
+import firebase from "firebase/app";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5k4AYDoYQ0Wxpy-T1y1xxS9a8klUFqh8",
+  authDomain: "picmediasite-dev.firebaseapp.com",
+  projectId: "picmediasite-dev",
+  storageBucket: "picmediasite-dev.appspot.com",
+  messagingSenderId: "95700240409",
+  appId: "1:95700240409:web:a4452362557f8984fbc288",
+  measurementId: "G-NPJX7BWV40",
+};
 
-import LandingScreen from './components/auth/Landing'
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}
+    <NavigationContainer>
+      {/* Rest of your app code */}
       <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
-
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { View, Button, TextInput } from "react-native";
+import firebase from "firebase";
+import "firebase/firestore";
 
 export class Register extends Component {
   constructor(props) {
@@ -11,10 +13,21 @@ export class Register extends Component {
       name: "",
     };
 
-    this.onSignup = this.onSignup.bind(this);
+    this.onSigngUp = this.onSignUp.bind(this);
   }
 
-  onSignup() {}
+  onSignUp() {
+    const { email, password, name } = this.state;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
